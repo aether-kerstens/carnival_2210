2e5401ee448b7ede13f3d07b695b023f1dd74cd2
Aether Kerstens <philippekerstens@gmail.com>
2022-11-14 08:53:23 -0800

    Finish iteration 1

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -4,11 +4,23 @@ class Visitor
   def initialize(name, hieght, spending_money)
     @name = name
     @hieght = hieght
-    @spending_money = spending_money
+    @spending_money = set_money(spending_money)
     @preferences = []
   end
 
+  def set_money(spending_money)
+    spending_money.delete('$').to_f
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
   def tall_enough?(required_hieght)
-    required_hieght >= @hieght
+    if required_hieght > @hieght
+      false
+    else
+      true
+    end
   end
 end
