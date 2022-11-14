d9c6d495ad2c3e0b1df03e591870805bfa1dce4d
Aether Kerstens <philippekerstens@gmail.com>
2022-11-14 09:32:57 -0800

    Create ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,16 @@
 class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log, :total_revenue
 
+  def initialize(ride_data)
+    @name = ride_data[:name]
+    @min_height = ride_data[:min_height]
+    @admission_fee = ride_data[:admission_fee]
+    @excitement = ride_data[:excitement]
+    @rider_log = {}
+    @total_revenue = 0
+  end
+
+  def board_rider
+    
+  end
 end
