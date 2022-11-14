1ac08a164c2244938f02ef319db01c2000004366
Aether Kerstens <philippekerstens@gmail.com>
2022-11-14 08:48:15 -0800

    Create visitor class

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,14 @@
 class Visitor
+  attr_reader :name, :hieght, :spending_money, :preferences
 
+  def initialize(name, hieght, spending_money)
+    @name = name
+    @hieght = hieght
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+  def tall_enough?(required_hieght)
+    required_hieght >= @hieght
+  end
 end
